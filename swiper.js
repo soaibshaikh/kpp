865c6b832707c7ee5639299a7b9814b085538c09
soaibshaikh <soaibshaikh1999@gmail.com>
2024-02-07 16:14:01 +0530

    responsive-fixes

--- swiper.js
+++ swiper.js
@@ -112,10 +112,10 @@ var TrandingSlider = new Swiper(".tranding-slider1", {
 var swiper = new Swiper(".mySwiper-leader", {
   slidesPerView: 1,
   spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
