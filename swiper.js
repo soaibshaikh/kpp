c2bfefcd25eda2075dc0fc2a6868af7f448d7c37
soaibshaikh <soaibshaikh1999@gmail.com>
2024-02-07 07:28:12 +0530

    first commit

---
+++ swiper.js
@@ -0,0 +1,153 @@
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  slidesPerView: 1,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  direction: "horizontal",
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 1,
+  centeredSlides: true,
+  // loop: true,
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  // slidesPerView: 4,
+  spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+  loop: true,
+  autoplay: {
+    delay: 25000,
+    // disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// events sliders
+
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var TrandingSlider = new Swiper(".tranding-slider1", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// leaders slider
+var swiper = new Swiper(".mySwiper-leader", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
+
+// navbar js
+const navbar = document.querySelector(".custom-header");
+const openBtn = document.querySelector(".navbar-open");
+
+openBtn.addEventListener("click", (event) => {
+  navbar.classList.toggle("open");
+});
+
+// Ubed JS for Nav Toggle
